34c534318bd0ffc153b476d6ad3f736f634f6053
Layona-x <109739779+Layona-x@users.noreply.github.com>
2022-09-06 15:30:14 +0300

    Add files via upload

--- models/photo.js
+++ models/photo.js
@@ -1,1 +1,27 @@
 
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const photoSchema = new Schema({
+  title:{
+    type:String,
+    require:true
+  },
+  description:{
+    type:String,
+    require:true,
+  },
+  photo:{
+    type:String,
+    require:true
+  },
+  username:{
+    type:String,
+    require:true,
+  },
+  userId:{
+    type:String,
+    require:true
+  },
+});
+module.exports = mongoose.model("Photo",photoSchema)
